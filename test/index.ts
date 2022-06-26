6837e8c3cb66f33b45a595a632cee348d4f796d5
otariidae <otariidae@users.noreply.github.com>
2022-06-27 03:12:03 +0900

    refactor: use native test runner

--- test/index.ts
+++ test/index.ts
@@ -1,59 +1,63 @@
+import test from "node:test"
 import { strict as assert } from "assert"
 import { ke2dairanization } from "../src"
 
-describe("test words", () => {
-  it("松平 健", async () => {
+test("test words", async (t) => {
+  await t.test("松平 健", async () => {
     assert.equal(await ke2dairanization("松平 健"), "ケツダイラ マン")
   })
-  it("ポール マッカートニー", async () => {
+  await t.test("ポール マッカートニー", async () => {
     assert.equal(
       await ke2dairanization("ポール マッカートニー"),
       "マール ポッカートニー"
     )
   })
-  it("森 進一", async () => {
+  await t.test("森 進一", async () => {
     assert.equal(await ke2dairanization("森 進一"), "シリ モンイチ")
   })
-  it("デーモン 小暮", async () => {
+  await t.test("デーモン 小暮", async () => {
     assert.equal(await ke2dairanization("デーモン 小暮"), "コーモン デグレ")
   })
-  it("チェ ゲバラ", async () => {
+  await t.test("チェ ゲバラ", async () => {
     assert.equal(await ke2dairanization("チェ ゲバラ"), "ゲ チェバラ")
   })
-  it("カトウ アイ", async () => {
+  await t.test("カトウ アイ", async () => {
     assert.equal(await ke2dairanization("加藤 あい"), "アトウ カイ")
   })
-  it("アトウ カイ", async () => {
+  await t.test("アトウ カイ", async () => {
     assert.equal(await ke2dairanization("阿藤 快"), "カトウ アイ")
   })
-  it("ハリー ジェームズ ポッター", async () => {
+  await t.test("ハリー ジェームズ ポッター", async () => {
     assert.equal(
       await ke2dairanization("ハリー ジェームズ ポッター"),
       "ポリー ジェームズ ハッター"
     )
   })
-  it("アルバス パーシバル ウルフリック ブライアン ダンブルドア", async () => {
-    assert.equal(
-      await ke2dairanization(
-        "アルバス パーシバル ウルフリック ブライアン ダンブルドア"
-      ),
-      "ダルバス パーシバル ウルフリック ブライアン アンブルドア"
-    )
-  })
+  await t.test(
+    "アルバス パーシバル ウルフリック ブライアン ダンブルドア",
+    async () => {
+      assert.equal(
+        await ke2dairanization(
+          "アルバス パーシバル ウルフリック ブライアン ダンブルドア"
+        ),
+        "ダルバス パーシバル ウルフリック ブライアン アンブルドア"
+      )
+    }
+  )
 
-  it("1 word", async () => {
+  await t.test("1 word", async () => {
     assert.equal(await ke2dairanization("松平"), "マツダイラ")
   })
-  it("space + 1 word", async () => {
+  await t.test("space + 1 word", async () => {
     assert.equal(await ke2dairanization(" 松平"), "マツダイラ")
   })
-  it("1 word + space", async () => {
+  await t.test("1 word + space", async () => {
     assert.equal(await ke2dairanization(" 松平 "), "マツダイラ")
   })
-  it("space", async () => {
+  await t.test("space", async () => {
     assert.equal(await ke2dairanization(" "), "")
   })
-  it("empty", async () => {
+  await t.test("empty", async () => {
     assert.equal(await ke2dairanization(""), "")
   })
 })
